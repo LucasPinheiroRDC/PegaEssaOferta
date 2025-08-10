79255db6718d58c4b8095b4feffb03701daf91bc
PRCLucas <107966561+LucasPinheiroRDC@users.noreply.github.com>
2025-08-10 12:27:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Arquivo de scripts para a landing page
+
+// Exemplo simples para verificar se o arquivo está sendo carregado corretamente
+console.log("O script.js foi carregado com sucesso!");
+
+// Exemplo de como você poderia adicionar um evento de clique a um botão
+// document.addEventListener('DOMContentLoaded', () => {
+//     const ctaButton = document.querySelector('.cta-button');
+//     ctaButton.addEventListener('click', (event) => {
+//         event.preventDefault(); // Impede o comportamento padrão do link
+//         alert('O botão foi clicado!');
+//     });
+// });
